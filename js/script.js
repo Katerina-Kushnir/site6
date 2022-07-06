05df36f1d818aa979e4fa20e8cbf9f92df1eadb1
Katerina-Kushnir <kushnirkatya25@gmail.com>
2022-07-06 17:37:58 +0300

    site6

---
+++ js/script.js
@@ -0,0 +1,43 @@
+let swiper = new Swiper(".mySwiper", {
+    autoplay: {
+        delay: 1500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+jQuery(($) => {
+    $('.select').on('click', '.select__head', function () {
+        if ($(this).hasClass('open')) {
+            $(this).removeClass('open');
+            $(this).next().fadeOut();
+        } else {
+            $('.select__head').removeClass('open');
+            $('.select__list').fadeOut();
+            $(this).addClass('open');
+            $(this).next().fadeIn();
+        }
+    });
+
+    $('.select').on('click', '.select__item', function () {
+        $('.select__head').removeClass('open');
+        $(this).parent().fadeOut();
+        $(this).parent().prev().text($(this).text());
+        $(this).parent().prev().prev().val($(this).text());
+    });
+
+    $(document).click(function (e) {
+        if (!$(e.target).closest('.select').length) {
+            $('.select__head').removeClass('open');
+            $('.select__list').fadeOut();
+        }
+    });
+});
+
+$(function(){
+	$("#phone").mask("+7 (999) 999-9999");
+});
+
